2721391ddee02f7165fc0b87c6dc9aed1def5840
Jhoy Kallebe <kallebejhoy@gmail.com>
2024-03-10 17:02:05 -0300

    Complete New Project Page

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,11 +1,12 @@
-import stylesPages from './pages.module.css'
+import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
     return (
-        <div className={stylesPages.container}>
+        <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto" />
         </div>
     )
 }
